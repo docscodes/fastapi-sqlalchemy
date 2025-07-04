5c15b17749e2672a3086ebc409efda85649b5979
docscodes <docscodes@gmail.com>
2025-07-04 15:38:01 -0400

    add routes

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from collections.abc import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from .models import Base
+from models import Base
 
 DATABASE_URL = "sqlite+aiosqlite:///sqlalchemy.db"
 engine = create_async_engine(DATABASE_URL)
